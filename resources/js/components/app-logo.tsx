45800439112ccf46894ea550254a5dd368a40bd1
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2025-11-25 23:56:21 +0100

    add cartoonio logos

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,8 @@
-import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center p-1">
+                <img src="/cartoonio192.png" alt="Logo" className="size-full object-contain" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
